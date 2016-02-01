848284db5dcaf6959fd34a480b5d43f867804cfa
Khaled Ahmad <koahmad98@gmail.com>
2016-01-31 18:55:19 -0800

    hello

--- Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/MainActivity.java
+++ Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Set up database
         dbHelper = new DatabaseHelper(this);
-        //dbHelper.remakeTaskTable();
+        dbHelper.remakeTaskTable();
         populateTaskList();
     }
 
@@ -83,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
                         false);
 
                 // update list of tasks
-                populateTaskList();
             }
         }
     }
