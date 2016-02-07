e4bcc176b13dc07e37bacbc37d3e0b4fea67f836
Khaled Ahmad <koahmad98@gmail.com>
2016-02-06 16:23:19 -0800

    Created XML and Java files for tabs -tasks, notes, and performance

---
+++ Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/NotesFragment.java
@@ -0,0 +1,7 @@
+package com.cse110.team1.todoapp;
+
+/**
+ * Created by khaledahmad on 2/6/16.
+ */
+public class NotesFragment {
+}
---
+++ Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/TasksFragment.java
@@ -0,0 +1,28 @@
+package com.cse110.team1.todoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by khaledahmad on 2/6/16.
+ */
+public class TasksFragment extends Fragment{
+    public TasksFragment(){
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tasks_fragment, container, false);
+    }
+}
