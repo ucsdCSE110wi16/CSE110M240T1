06516346f13ad39cb2ffa5f924eeb1a674bdf95b
Ameer Muhtaseb <amuhtase@ucsd.edu>
2016-03-03 13:46:17 -0800

    Begin NewNoteActivity

--- Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/NewNoteActivity.java
+++ Application/ToDoApp/app/src/main/java/com/cse110/team1/todoapp/NewNoteActivity.java
@@ -14,25 +14,20 @@ public class NewNoteActivity extends AppCompatActivity{
 
     // Called when user clicks save button
     public void saveNote (View view) {
-//        EditText nameBox = (EditText) findViewById(R.id.task_field);
-//        EditText detailBox = (EditText) findViewById(R.id.detail_field);
-//        SeekBar sbar = (SeekBar) findViewById(R.id.progress_seek_bar);
-//
-//        // Create an intent to attach when this activity finishes.  The new task entry is still
-//        // added in this activity, but the intent remains in case MainActivity wants it.  Also,
-//        // this activity is launched with startActivityForResult anyway because MainActivity needs
-//        // to know when to refresh the ListView
-//        Intent result = new Intent();
-//        result.putExtra(TASK_DESCRIPTION, nameBox.getText().toString());
-//        result.putExtra(TASK_DETAILS, detailBox.getText().toString());
-//        result.putExtra(TASK_PROGRESS, sbar.getProgress());
-//        setResult(RESULT_OK, result);
-//
-//        boolean isDone = false;
-//        if (sbar.getProgress() >= 100) {
-//            isDone = true;
-//        }
-//        // add new entry to table
+        //EditText nameBox = (EditText) findViewById(R.id.task_field);
+        //EditText detailBox = (EditText) findViewById(R.id.detail_field);
+
+        // Create an intent to attach when this activity finishes.  The new task entry is still
+        // added in this activity, but the intent remains in case MainActivity wants it.  Also,
+        // this activity is launched with startActivityForResult anyway because MainActivity needs
+        // to know when to refresh the ListView
+        Intent result = new Intent();
+        result.putExtra(NOTE_DESCRIPTION, nameBox.getText().toString());
+        result.putExtra(NOTE_DETAILS, detailBox.getText().toString());
+        setResult(RESULT_OK, result);
+
+
+        // add new entry to table
 //        DatabaseHelper dbHelper = new DatabaseHelper(this);
 //        dbHelper.insertTask(result.getExtras().getString(NewTaskActivity.TASK_DESCRIPTION),
 //                result.getExtras().getString(NewTaskActivity.TASK_DETAILS),
